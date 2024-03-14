1502f753115c613c0e72f0e281975db9af63c614
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-03-13 22:23:18 -0300

    agregue el puerto 4000 para el backend

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-import express from 'express';
-console.log('Hola mundo')
+import express from "express";
+console.log("Hola mundo");
+
+const app = express();
+app.set('port', 4000)
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'))
+})
