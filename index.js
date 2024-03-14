0889bd1a2b293edab98204ceb98f43655859fc3c
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-03-14 17:59:12 -0300

    agregue los middlewares cors, morgan y dotenv

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import morgan from "morgan";
 console.log("Hola mundo");
 
 const app = express();
-app.set('port', process.env.PORT || 4000)
-app.listen(app.get('port'), () => {
-    console.log(app.get('port'))
-})
+
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"));
+});
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true})
+);
