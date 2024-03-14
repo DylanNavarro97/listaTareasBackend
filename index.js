1b34604d829190ddf42b73c393bcfcebfed4cabc
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-03-14 17:37:30 -0300

    agregue un puerto y lo utilizo desde index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 console.log("Hola mundo");
 
 const app = express();
-app.set('port', 4000)
+app.set('port', process.env.PORT || 4000)
 app.listen(app.get('port'), () => {
     console.log(app.get('port'))
 })
