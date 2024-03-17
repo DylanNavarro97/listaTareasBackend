3418d92e0dab0844529eb6ced7e9c13054dde5c6
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-03-17 14:54:13 -0300

    controlador de crear producto realizado

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -4,3 +4,18 @@ export const listarTareas = (req, res) => {
     console.log('desde listar tareas')
     res.send('Enviar lista de tareas...')
 }
+
+export const crearTarea = async (req, res) => {
+    try {
+        const tareaNueva = new Tarea(req.body);
+        await tareaNueva.save()
+        res.status(201).json({
+            mensaje: "La tarea fue creada correctamente"
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({
+            mensaje: "No se pudo procesar la solicitud de crear tarea"
+        })
+    }
+}
